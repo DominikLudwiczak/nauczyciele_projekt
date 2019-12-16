35cc9d1fa6c44667c839bb9175594e31c3de011f
KILLforSKILL89 <ludek077@gmail.com>
2019-12-16 10:45:44 +0100

    final_wroking

--- funkcje.h
+++ funkcje.h
@@ -3,7 +3,6 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <sstream>
 #include <conio.h>
 
 using namespace std;
@@ -21,20 +20,23 @@ void check()
 	{
 		cout << "Obecny - 1	Nieobecny - 0" << endl;
 		getline(nauczyciele, imie);
-		do
+		if (imie != "")
 		{
-			cout << imie << " ";
-			cin >> check;
-			if (check != "0" && check != "1")
+			do
 			{
-				cout << "wprowadz 1 lub 0!" << endl;
-			}
-		} while (check != "0" && check != "1");
-		if (check == "1")
-			obecnosc << imie << ">OBECNY" << endl;
-		else
-			obecnosc << imie << ">NIEOBECNY" << endl;
-		system("cls");
+				cout << imie << " ";
+				cin >> check;
+				if (check != "0" && check != "1")
+				{
+					cout << "wprowadz 1 lub 0!" << endl;
+				}
+			} while (check != "0" && check != "1");
+			if (check == "1")
+				obecnosc << imie << ">OBECNY" << endl;
+			else
+				obecnosc << imie << ">NIEOBECNY" << endl;
+			system("cls");
+		}
 	}
 	nauczyciele.close();
 	obecnosc.close();
@@ -48,28 +50,37 @@ void zlicz_glosy(int obecni_count)
 	ofstream z_glosy("pliki/zliczone_glosy.txt");
 	int tak = 0, nie = 0, nwm = 0;
 	string nazwisko;
+	vector<string> file;
 
 	while (!glosy.eof())
 	{
 		getline(glosy, nazwisko);
-		istringstream split(nazwisko);
-		vector<string> tokens;
-		for (string each; getline(split, each, '>'); tokens.push_back(each));
-		if (tokens[1] == "TAK")
-			tak++;
-		else if (tokens[1] == "NIE")
-			nie++;
-		else
-			nwm++;
+		file.push_back(nazwisko);
 	}
+
+	for(int i=0; i < file.size(); i++)
+	{
+		if (file.at(i) != "")
+		{
+			if (file.at(i).at(file.at(i).length() - 3) == (const char)'T')
+				tak++;
+			else if (file.at(i).at(file.at(i).length() - 3) == (const char)'N')
+				nie++;
+			else
+				nwm++;
+		}
+	}
+	
 	if (tak + nie + nwm == obecni_count)
 	{
 		z_glosy << "iloœæ g³osów na TAK: " << tak << endl;
 		z_glosy << "iloœæ g³osów na NIE: " << nie << endl;
 		z_glosy << "iloœæ wstrzymania siê od g³osów: " << nwm;
+		cout << "Glosowanie zakonczone sukcesem!" << endl;
 	}
 	else
 		cout << "blad glosowania! Niepoprawna liczba oddanych glosow!";
+	system("pause");
 	glosy.close();
 }
 
@@ -79,6 +90,7 @@ void glosowanie()
 	ifstream obecnosc;
 	obecnosc.open("pliki/obecnoœæ.txt");
 	string nauczyciel;
+	vector<string> file;
 	string glos;
 	ofstream glosowanie("pliki/glosowanie.txt");
 	int obecni_count = 0;
@@ -86,29 +98,37 @@ void glosowanie()
 	while (!obecnosc.eof())
 	{
 		getline(obecnosc, nauczyciel);
-		istringstream split(nauczyciel);
-		vector<string> tokens;
-		for (string each; getline(split, each, '>'); tokens.push_back(each));
-		if (tokens[1] == "OBECNY")
+		file.push_back(nauczyciel);
+	}
+
+	for (int i=0; i < file.size(); i++)
+	{
+		if (file.at(i) != "")
 		{
-			obecni_count++;
-			cout << "Prosze wpisac 'TAK', 'NIE' lub 'NWM dla wstrzymania sie od glosu'" << endl;
-			do
+			if (file.at(i).at(file.at(i).length() - 7) == (const char)'>')
 			{
-				cout << tokens[0] << " ";
-				cin >> glos;
-				if (glos != "TAK" && glos != "NIE" && glos != "NWM")
+				obecni_count++;
+				cout << "Prosze wpisac 'TAK', 'NIE' lub 'NWM dla wstrzymania sie od glosu'" << endl;
+				do
 				{
-					cout << "Wprowadz poprawny glos!" << endl;
+					for (int j = 0; j < file.at(i).size()-7; j++)
+						nauczyciel = nauczyciel+file.at(i).at(j);
+					cout << nauczyciel << " ";
+					cin >> glos;
+					if (glos != "TAK" && glos != "NIE" && glos != "NWM")
+					{
+						cout << "Wprowadz poprawny glos!" << endl;
+					}
+				} while (glos != "TAK" && glos != "NIE" && glos != "NWM");
+				if (glos == "NWM")
+				{
+					glos = "WSYTRZYMA£ SIÊ";
 				}
-			} while (glos != "TAK" && glos != "NIE" && glos != "NWM");
-			if (glos == "NWM")
-			{
-				glos = "WSYTRZYMA£ SIÊ";
+				glosowanie << nauczyciel << ">" << glos << endl;
 			}
-			glosowanie << tokens[0] << ">" << glos << endl;
+			nauczyciel = "";
+			system("cls");
 		}
-		system("cls");
 	}
 	glosowanie.close();
 	obecnosc.close();
